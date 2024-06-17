fcba474f691b40c9ec69482fdf9574090f765dbf
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-06-17 21:51:58 +0530

    Updating the vite config to include webconfig in build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "tailwindcss";
+import tailwindcss from "tailwindcss"
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'web.config', // path to your web.config
+          dest: '' // destination directory, '' means root of the build directory
+        }
+      ]
+    })
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss],
